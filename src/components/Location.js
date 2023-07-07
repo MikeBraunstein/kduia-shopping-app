374d515c1eefeea274c9e31de6af7f0a16aad210
MikeBraunstein <mikebraunstein@duck.com>
2023-07-07 00:43:53 -0400

    Updated Location.js typo in the return under the select where the onChange had onCHange.

--- src/components/Location.js
+++ src/components/Location.js
@@ -14,7 +14,7 @@ const Location = () => {
 
 return (
       <div className='alert alert-secondary'> Location {
-    <select name="Location" id="Location" onCHange={event=>changeLocation(event.target.value)}>
+    <select name="Location" id="Location" onChange={event=>changeLocation(event.target.value)}>
         <option value="£">Uk(£)</option>
         <option value="₹">India(₹)</option>
         <option value="€">Europe(€)</option>
